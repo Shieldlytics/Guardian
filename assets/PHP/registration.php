f8d4f574a5dacb0c57c7319b07190767dcaf78b3
Ernest Pena <erniep@mdanderson.org>
2024-03-14 12:53:07 -0500

    more changes

--- assets/PHP/registration.php
+++ assets/PHP/registration.php
@@ -1,17 +1,17 @@
-https://guardian1.azurewebsites.net/signup.html
-// function getConnection() {
-//     $serverName = "tcp:guardian-dev-db.database.windows.net,1433";
-//     $database = "GUARDIAN-DEV";
-//     $username = "GUARDIAN";
-//     $password = "Sh13ldlyt1c$";
-//     try {
-//         $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
-//         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//         return $conn;
-//     } catch (PDOException $e) {
-//         die("Connection failed: " . $e->getMessage());
-//     }
-// }
+<?php
+function getConnection() {
+    $serverName = "tcp:guardian-dev-db.database.windows.net,1433";
+    $database = "GUARDIAN-DEV";
+    $username = "GUARDIAN";
+    $password = "Sh13ldlyt1c$";
+    try {
+        $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
 
 // if(isset($_POST["method"])) {
 //     if(isset($_POST["registerNewUser"])) {
